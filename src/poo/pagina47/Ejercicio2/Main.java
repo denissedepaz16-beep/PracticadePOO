8051a11102f0d4cfed74e52b96567722b52b8f3a
MINEDUCYT <MINEDUCYT@YAMICOMPU>
2026-06-10 07:08:40 -0600

    Karla Yamileth Ceren Perez - Ejercicio 2, Pag.47 finalizado

---
+++ src/poo/pagina47/Ejercicio2/Main.java
@@ -0,0 +1,11 @@
+package poo.pagina47.Ejercicio2;
+public class Main {
+
+    public static void main(String[] args) {
+
+        Empleado emp = new Empleado("Carlos", 400);
+
+        emp.registrar();
+        emp.calcularSueldo();
+    }
+}
