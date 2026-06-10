4ca25add973703a240b138eade70c7f5ce482edb
MINEDUCYT <MINEDUCYT@YAMICOMPU>
2026-06-10 04:45:58 -0600

    Karla Yamileth Ceren Perez- Ejercicio 1, pag.43 finalizado

---
+++ src/poo/Pagina43/Futbolista.java
@@ -0,0 +1,22 @@
+package poo.Pagina43;
+
+public class Futbolista extends Persona {
+    private int dorsal;
+    private final String demarcacion;
+
+
+    public Futbolista(int id, String nombre, String apellidos, int edad, int dorsal, String demarcacion) {
+        super(id, nombre, apellidos, edad);
+        this.dorsal = dorsal;
+        this.demarcacion = demarcacion;
+    }
+
+
+    public void jugarPartido() {
+        System.out.println("El futbolista " + getNombre() + " está jugando un partido como " + demarcacion + ".");
+    }
+
+    public void entrenar() {
+        System.out.println("El futbolista " + getNombre() + " está entrenando en la cancha.");
+    }
+}
